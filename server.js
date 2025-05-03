e4b885e798789094227dd43f528bfb84878f920c
Vilate <viknapp22@gmail.com>
2025-05-03 17:42:43 -0600

    added cors package

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
-var express = require('express');
-var app = express();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
 const port = process.env.PORT || 3000
+
+app.use(cors());
 
 app.use('/', require('./routes'));
 
